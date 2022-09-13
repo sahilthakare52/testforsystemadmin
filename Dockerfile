d1c3eeb7113177e7b34cb464ffa9e69e3413383c
root <root@ip-172-31-19-145.ec2.internal>
2022-09-13 15:04:51 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM webdevops/php-nginx
+
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" && \
+    php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;" && \
+    php composer-setup.php && \
+    php -r "unlink('composer-setup.php');" && \
+    cp composer.phar /usr/local/bin/composer 
+RUN php composer.phar create-project --prefer-dist cakephp/app:4.* cms -n 
+CMD cms/bin/cake server -H 0.0.0.0
+
+
---
+++ app.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+echo "checking the prerequisites- nginx and docker installations..."
+## checking if the nginx is installed if not install the nginx on server
+sudo apt-update -y
+which nginx
+output=$?
+[ $output = 0 ] && echo "nginx is already installed moving to the next step" || sudo apt install nginx -y 
+
+which docker
+output=$?
+
+[ $output = 0 ] && echo "docker is already installed moving to the next step" || sh docker_install.sh
+
+##building the application using docker compose....
+
+echo "building the application using docker compose......."
+
+docker compose up -d 
+
+## adding server block to transfer all the http requests to https for https://demo.algonquainlanguages.ca
+touch /etc/nginx/conf.d/demo.conf
+echo "server {
+    listen 80;
+    server_name demo.algonquainlanguages.ca;
+    return 301 https://demo.algonquainlanguages.ca$request_uri;
+}" >> /etc/nginx/conf.d/demo.conf
+
+echo "server {
+    listen 443 ssl;
+
+    server_name demo.algonquainlanguages.ca;
+
+    ssl_certificate ...;
+    ssl_certificate_key ...;
+
+    location / {
+        proxy_pass http://127.0.0.1:9000;
+    }
+}" >> /etc/nginx/conf.d/demo.conf
