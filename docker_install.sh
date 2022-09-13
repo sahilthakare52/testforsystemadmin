d4afc62ad610adacffb04c2dc7532bb1a0b23c49
sahilthakare52 <102465284+sahilthakare52@users.noreply.github.com>
2022-09-13 13:30:50 -0400

    added sudo

--- docker_install.sh
+++ docker_install.sh
@@ -8,9 +8,9 @@ sudo apt-get install -y \
 
 sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-echo \
+sudo echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
